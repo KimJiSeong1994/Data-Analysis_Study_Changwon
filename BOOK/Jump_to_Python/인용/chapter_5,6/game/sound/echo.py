95f0ce4a297a1e263bf55f69c5d78e537e551b98
KimJiSeong1994 <js_2109@naver.com>
2019-10-25 12:29:47 +0900

    Add files via upload

---
+++ BOOK/Jump_to_Python/인용/chapter_5,6/game/sound/echo.py
@@ -0,0 +1,14 @@
+
+# coding: utf-8
+
+# In[1]:
+
+# echo.py
+def echo_test():
+    print ("echo")
+
+
+# In[ ]:
+
+
+
---
+++ BOOK/Jump_to_Python/인용/chapter_5,6/game/sound/render.py
@@ -0,0 +1,16 @@
+
+# coding: utf-8
+
+# In[1]:
+
+# render.py
+from ..sound.echo import echo_test
+
+def render_test():
+    print ("render")
+    echo_test()
+
+# In[ ]:
+
+
+
